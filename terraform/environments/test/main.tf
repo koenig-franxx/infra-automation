38415bf9df9ce41dcf7047bd5e6ec298ed2b5012
DiegoMiguelanez <miguelanez.diego@gmail.com>
2024-10-08 06:46:19 +0200

    Added more vars plus functional deployment of test environment using module node templating for multiple node creation such as kubernetes master, slaves and grafana nodes

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -1,5 +1,18 @@
-
-module "test_node" {
+module "kubernetes_master" {
   source = "../../modules/node"  # Ruta hacia el módulo node
   aws_instance_type = "t3.medium"
+  vm_name_prefix = "hokage-k8s-master"
+}
+
+module "kubernetes_worker" {
+  source = "../../modules/node"  # Ruta hacia el módulo node
+  aws_instance_type = "t2.micro"
+  vm_name_prefix = "shinobi-k8s-worker"
+  vm_count = 2
+}
+
+module "grafana_node" {
+  source = "../../modules/node"  # Ruta hacia el módulo node
+  aws_instance_type = "t2.micro"
+  vm_name_prefix = "sharingan-grafana"
 }
